bf0a783c1991a987cc5f3ad893401ed8586545fa
Tyranowski <andretyranowski@gmail.com>
2024-11-29 21:55:36 +0100

    backend use sqlite now

--- models/index.js
+++ models/index.js
@@ -1,14 +1,10 @@
-const { Sequelize } = require("sequelize");
+const { Sequelize, DataTypes } = require("sequelize");
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    dialect: "mysql",
-  }
-);
+// Sequelize configuration for SQLite
+const sequelize = new Sequelize({
+  dialect: "sqlite",
+  storage: process.env.DB_STORAGE || "./database.sqlite", // SQLite database file location
+});
 
 // Test Connection
 (async () => {
